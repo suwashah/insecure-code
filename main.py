87946a9ecb964619437f83cdc85e7ffbbc9a7b0e
Carter Smith <carter.smith@stric.com.au>
2024-04-28 21:01:07 +0930

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import os
+
+aws_access_key_id = "AKIA6ODU5DHTS3UIPEVK"
+aws_secret_access_key = "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"
+
+def gimme_shell():
+    # RCE
+    directory = input("Enter the directory to list: ")
+    command = f"ls {directory}"  # Vulnerable to Command Injection
+    os.system(command)
+
+
+def gimme_files():
+    # Directory Traversal
+    user_input = input("Enter filename: ")
+    with open(user_input, 'r') as file:
+        content = file.read()
+
+
+def gimme_sql():
+    user_input = input("Enter your username: ")
+    query = "SELECT * FROM users WHERE username = '" + user_input + "';"
+    print(f"Your SQL command: {query}")
+
+
+def main():
+    gimme_shell()
+    gimme_files()
+    gimme_sql()
+
+
+if __name__ == "__main__":
+    main()
